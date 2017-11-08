cc52fd2bf8b9e47516a275cf284ff60eb0b72c20
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-11-08 20:50:15 +0100

    Add modal animation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,7 +25,12 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchaseable: false
+    purchaseable: false,
+    purchasing: false
+
+  }
+  purchaseHandler = () => {
+    this.setState({purchasing: true})
   }
   updatePurchaseState(ingredients) {
     const sum = Object.keys(ingredients)
@@ -76,7 +81,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -85,6 +90,7 @@ class BurgerBuilder extends Component {
           ingredientRemove={this.removeIngredientHandler}
           disabled={disabledInfo}
           purchaseable={this.state.purchaseable}
+          ordered={this.purchaseHandler}
           price={this.state.totalPrice}/>
       </Aux>
     );

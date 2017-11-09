cab343a143fa657010a0343ff06943ea459482ca
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-11-09 20:40:52 +0100

    Adding responive side drawer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classes from './Logo.css';
 
 
 const logo = (props) => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style={{height: props.height, marginBottom: props.marginBottom}}>
     <img src={burgerLogo} alt="MyBurger"/>
   </div>
 );

ac7b07653508385a012f03e17f1b4b3a1cdae227
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-11-13 12:42:40 +0100

    Update purchaseContinuesHandler to send post request with axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,8 @@ import Aux from '../../hoc/Auxiliary/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -38,7 +39,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinuelHandler = () => {
-    alert('You Continue!');
+    // alert('You Continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Mateusz Niestroj',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '31351',
+          country: 'Poland'
+        },
+        email: 'test@test.com'
+      },
+      deliverMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   updatePurchaseState(ingredients) {

f461358e2c1b80fd8779f6301b1dcd9776b15a72
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-11-17 18:48:49 +0100

    adding contact form

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
@@ -32,6 +34,9 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHander}
           checkoutContinued={this.checkoutContinuedHandler}/>
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          component={ContactData} />
       </div>
     )
   }
